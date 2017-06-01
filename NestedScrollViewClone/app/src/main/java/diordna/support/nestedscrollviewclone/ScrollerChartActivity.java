3ffc30c90861a46682bda81d9327a0e422048f17
yinhangfeng <darkwander.y@gmail.com>
2017-06-01 21:20:19 +0800

    Scroller chart

--- NestedScrollViewClone/app/src/main/java/diordna/support/nestedscrollviewclone/ScrollerChartActivity.java
+++ NestedScrollViewClone/app/src/main/java/diordna/support/nestedscrollviewclone/ScrollerChartActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 
@@ -54,23 +56,30 @@ public class ScrollerChartActivity extends AppCompatActivity {
         //xAxis.setValueFormatter(new MyCustomXAxisValueFormatter());
         //xAxis.addLimitLine(llXAxis); // add x-axis limit line
 
-        LimitLine ll1 = new LimitLine(1000f, "Edge");
-        ll1.setLineWidth(4f);
+        LimitLine ll1 = new LimitLine(1500f, "Max");
+        ll1.setLineWidth(2f);
         ll1.enableDashedLine(10f, 10f, 0f);
         ll1.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_TOP);
         ll1.setTextSize(10f);
 
-        LimitLine ll2 = new LimitLine(500f, "Edge");
-        ll2.setLineWidth(4f);
+        LimitLine ll2 = new LimitLine(500f, "Min");
+        ll2.setLineWidth(2f);
         ll2.enableDashedLine(10f, 10f, 0f);
         ll2.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_TOP);
         ll2.setTextSize(10f);
+
+        LimitLine ll3 = new LimitLine(2000f, "Over");
+        ll3.setLineWidth(2f);
+        ll3.enableDashedLine(10f, 10f, 0f);
+        ll3.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_TOP);
+        ll3.setTextSize(10f);
 
         YAxis leftAxis = mChart.getAxisLeft();
         leftAxis.removeAllLimitLines(); // reset all limit lines to avoid overlapping lines
         leftAxis.addLimitLine(ll1);
         leftAxis.addLimitLine(ll2);
-        leftAxis.setAxisMaximum(1600f);
+        leftAxis.addLimitLine(ll3);
+        leftAxis.setAxisMaximum(2100f);
         leftAxis.setAxisMinimum(0f);
         //leftAxis.setYOffset(20f);
         leftAxis.enableGridDashedLine(10f, 10f, 0f);
@@ -80,7 +89,7 @@ public class ScrollerChartActivity extends AppCompatActivity {
         leftAxis.setDrawLimitLinesBehindData(true);
 
         YAxis rightAxis = mChart.getAxisRight();
-        rightAxis.setAxisMaximum(5000);
+        rightAxis.setAxisMaximum(5000f);
         rightAxis.setAxisMinimum(0);
 
         initLineData();
@@ -122,8 +131,11 @@ public class ScrollerChartActivity extends AppCompatActivity {
             vset.setValueTextSize(9f);
             vset.setDrawValues(false);
             data.addDataSet(vset);
+        }
 
-            LineDataSet vcset = new LineDataSet(null, "VC");
+        LineDataSet vcset = (LineDataSet) data.getDataSetByIndex(2);
+        if (vcset == null && timeVc >= 0) {
+            vcset = new LineDataSet(null, "VC");
             vcset.setAxisDependency(YAxis.AxisDependency.RIGHT);
             vcset.setColor(Color.YELLOW);
             vcset.setCircleColor(Color.YELLOW);
@@ -136,9 +148,12 @@ public class ScrollerChartActivity extends AppCompatActivity {
             data.addDataSet(vcset);
         }
 
+
         data.addEntry(new Entry(time, l), 0);
         data.addEntry(new Entry(time, v), 1);
-        data.addEntry(new Entry(timeVc, vc), 2);
+        if (timeVc >= 0) {
+            data.addEntry(new Entry(timeVc, vc), 2);
+        }
 
         data.notifyDataChanged();
         mChart.notifyDataSetChanged();
@@ -151,14 +166,9 @@ public class ScrollerChartActivity extends AppCompatActivity {
         mChart.invalidate();
     }
 
-    public void test1(View v) {
-        reset();
-        final OverScroller overScroller = new OverScroller(this);
-        int vinit = 4000;
-        overScroller.fling(0, 0, 0, vinit, 0, 0, 0, 1000, 0, 500);
-
+    private void startAnimation(final OverScroller overScroller, float startL, float startV, final boolean vc) {
         final long startTime = AnimationUtils.currentAnimationTimeMillis();
-        addEntry(0, 0, vinit, 0, vinit);
+        addEntry(0, startL, startV, -1, -1);
 
         Runnable runnable = new Runnable() {
             double lastTime = startTime;
@@ -169,11 +179,16 @@ public class ScrollerChartActivity extends AppCompatActivity {
                     long currentAnimationTimeMillis = AnimationUtils.currentAnimationTimeMillis();
                     double time = currentAnimationTimeMillis - startTime;
                     double dt = currentAnimationTimeMillis - lastTime;
-                    double dl = overScroller.getCurrY() - lastL;
-                    double timeVC = (dt) / 2 + lastTime - startTime;
-                    Log.i(TAG, "run: " + currentAnimationTimeMillis + " " + time + " " + dt + " " + dl + " " + startTime);
+                    double currL = overScroller.getCurrY();
+                    double dl = currL - lastL;
+                    double timeVC = dt / 2 + lastTime - startTime;
+                    if (!vc) {
+                        timeVC = -1;
+                    }
+
                     lastTime = currentAnimationTimeMillis;
-                    lastL = overScroller.getCurrY();
+                    lastL = currL;
+
                     addEntry((float) time, overScroller.getCurrY(), overScroller.getCurrVelocity(), (float) timeVC, (float) (dl / dt * 1000));
                     mChart.postOnAnimation(this);
                 }
@@ -183,104 +198,152 @@ public class ScrollerChartActivity extends AppCompatActivity {
         mChart.postOnAnimation(runnable);
     }
 
-    public void test2(View v) {
+    // in => in
+    public void test1() {
         reset();
         final OverScroller overScroller = new OverScroller(this);
-        int vinit = 4000;
-        overScroller.startScroll(0, 0, 0, 1000, 2000);
+        int startL = 600;
+        int startV = 3000;
+        int minL = 500;
+        int maxL = 1500;
+        int overL = 500;
+        overScroller.fling(0, startL, 0, startV, 0, 0, minL, maxL, 0, overL);
 
-        final long startTime = AnimationUtils.currentAnimationTimeMillis();
-        addEntry(0, 0, vinit, 0, vinit);
-
-        Runnable runnable = new Runnable() {
-            double lastTime = startTime;
-            double lastL = 0;
-            @Override
-            public void run() {
-                if (overScroller.computeScrollOffset()) {
-                    long currentAnimationTimeMillis = AnimationUtils.currentAnimationTimeMillis();
-                    double time = currentAnimationTimeMillis - startTime;
-                    double dt = currentAnimationTimeMillis - lastTime;
-                    double dl = overScroller.getCurrY() - lastL;
-                    double timeVC = (dt) / 2 + lastTime - startTime;
-                    Log.i(TAG, "run: " + currentAnimationTimeMillis + " " + time + " " + dt + " " + dl + " " + startTime);
-                    lastTime = currentAnimationTimeMillis;
-                    lastL = overScroller.getCurrY();
-                    addEntry((float) time, overScroller.getCurrY(), overScroller.getCurrVelocity(), (float) timeVC, (float) (dl / dt * 1000));
-                    mChart.postOnAnimation(this);
-                }
-            }
-        };
-
-        mChart.postOnAnimation(runnable);
+        startAnimation(overScroller, startL, startV, false);
     }
 
-    public void test3(View v) {
+    // in => over
+    public void test2() {
         reset();
         final OverScroller overScroller = new OverScroller(this);
-        int vinit = 5000;
-        int startY = 500;
-        overScroller.fling(0, startY, 0, vinit, 0, 0, 0, 1000, 0, 500);
+        int startL = 1000;
+        int startV = 4000;
+        int minL = 500;
+        int maxL = 1500;
+        int overL = 500;
+        overScroller.fling(0, startL, 0, startV, 0, 0, minL, maxL, 0, overL);
 
-        final long startTime = AnimationUtils.currentAnimationTimeMillis();
-        addEntry(0, startY, vinit, 0, vinit);
-
-        Runnable runnable = new Runnable() {
-            double lastTime = startTime;
-            double lastL = 0;
-            @Override
-            public void run() {
-                if (overScroller.computeScrollOffset()) {
-                    long currentAnimationTimeMillis = AnimationUtils.currentAnimationTimeMillis();
-                    double time = currentAnimationTimeMillis - startTime;
-                    double dt = currentAnimationTimeMillis - lastTime;
-                    double dl = overScroller.getCurrY() - lastL;
-                    double timeVC = (dt) / 2 + lastTime - startTime;
-                    Log.i(TAG, "run: " + currentAnimationTimeMillis + " " + time + " " + dt + " " + dl + " " + startTime);
-                    lastTime = currentAnimationTimeMillis;
-                    lastL = overScroller.getCurrY();
-                    addEntry((float) time, overScroller.getCurrY(), overScroller.getCurrVelocity(), (float) timeVC, (float) (dl / dt * 1000));
-                    mChart.postOnAnimation(this);
-                }
-            }
-        };
-
-        mChart.postOnAnimation(runnable);
+        startAnimation(overScroller, startL, startV, false);
     }
 
-    public void test4(View v) {
+    // over => same over
+    public void test3() {
         reset();
         final OverScroller overScroller = new OverScroller(this);
-        int vinit = 5000;
-        overScroller.springBack(0, 0, 0, 0, 500, 1000);
+        int startL = 0;
+        int startV = 10;
+        int minL = 500;
+        int maxL = 1500;
+        int overL = 500;
+        overScroller.fling(0, startL, 0, startV, 0, 0, minL, maxL, 0, overL);
 
-        final long startTime = AnimationUtils.currentAnimationTimeMillis();
-        addEntry(0, 0, vinit, 0, vinit);
-
-        Runnable runnable = new Runnable() {
-            double lastTime = startTime;
-            double lastL = 0;
-            @Override
-            public void run() {
-                if (overScroller.computeScrollOffset()) {
-                    long currentAnimationTimeMillis = AnimationUtils.currentAnimationTimeMillis();
-                    double time = currentAnimationTimeMillis - startTime;
-                    double dt = currentAnimationTimeMillis - lastTime;
-                    double dl = overScroller.getCurrY() - lastL;
-                    double timeVC = (dt) / 2 + lastTime - startTime;
-                    Log.i(TAG, "run: " + currentAnimationTimeMillis + " " + time + " " + dt + " " + dl + " " + startTime);
-                    lastTime = currentAnimationTimeMillis;
-                    lastL = overScroller.getCurrY();
-                    addEntry((float) time, overScroller.getCurrY(), overScroller.getCurrVelocity(), (float) timeVC, (float) (dl / dt * 1000));
-                    mChart.postOnAnimation(this);
-                }
-            }
-        };
-
-        mChart.postOnAnimation(runnable);
+        startAnimation(overScroller, startL, startV, false);
     }
 
-    public void test5(View v) {
+    // over => in
+    public void test4() {
         reset();
+        final OverScroller overScroller = new OverScroller(this);
+        int startL = 0;
+        int startV = 4000;
+        int minL = 500;
+        int maxL = 1500;
+        int overL = 500;
+        overScroller.fling(0, startL, 0, startV, 0, 0, minL, maxL, 0, overL);
+
+        startAnimation(overScroller, startL, startV, false);
+    }
+
+    // over => other over
+    public void test5() {
+        reset();
+        final OverScroller overScroller = new OverScroller(this);
+        int startL = 100;
+        int startV = 6000;
+        int minL = 500;
+        int maxL = 1500;
+        int overL = 500;
+        overScroller.fling(0, startL, 0, startV, 0, 0, minL, maxL, 0, overL);
+
+        startAnimation(overScroller, startL, startV, false);
+    }
+
+    public void test6() {
+    }
+
+    public void test7() {
+        reset();
+        final OverScroller overScroller = new OverScroller(this);
+
+        int startL = 500;
+        float startV;
+        int minL = 0;
+        int maxL = 1000;
+        int overL = 500;
+        overScroller.springBack(0, startL, 0, 0, minL, maxL);
+        startV = overScroller.getCurrVelocity();
+
+        startAnimation(overScroller, startL, startV, false);
+    }
+
+    public void test8() {
+        reset();
+        final OverScroller overScroller = new OverScroller(this);
+
+        int startL = 0;
+        int dl = 1000;
+        int minL = 0;
+        int maxL = 1000;
+        int overL = 500;
+        int duration = 3000;
+        overScroller.startScroll(0, startL, 0, dl, duration);
+        float startV = overScroller.getCurrVelocity();
+
+        startAnimation(overScroller, startL, startV, true);
+    }
+
+    public void test9() {
+        reset();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.test, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+        case R.id.test1:
+            test1();
+            return true;
+        case R.id.test2:
+            test2();
+            return true;
+        case R.id.test3:
+            test3();
+            return true;
+        case R.id.test4:
+            test4();
+            return true;
+        case R.id.test5:
+            test5();
+            return true;
+        case R.id.test6:
+            test6();
+            return true;
+        case R.id.test7:
+            test7();
+            return true;
+        case R.id.test8:
+            test8();
+            return true;
+        case R.id.test9:
+            test9();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
